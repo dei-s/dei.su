4715fed9e070821e955d7a253994f915944670f8
ya <ya@dei.su>
2018-11-27 15:00:18 +0300

    Fix header.js

--- js/header.js
+++ js/header.js
@@ -5,12 +5,12 @@ if (typeof elements != "undefined" && elements.length > 0) {
 	var sLogo = document.createElement('div');
 	sLogo.className = 's-header_logo';
 	sLogo.id = 's-header-logo';
-	sLogo.innerHTML = '<img class="s-header_logo__img" src="img/logo.svg">';
+	sLogo.innerHTML = '<img class="s-header_logo__img" src="/img/logo.svg">';
 	sHeader.appendChild(sLogo);
 
 	var sHeaderMenu = document.createElement('ul');
 	sHeaderMenu.className = 's-header-menu';
 	sHeaderMenu.id = 's-header-menu';
-	sHeaderMenu.innerHTML = '<li><a href="edu">Образование</a></li><li><a href="projects">Проекты</a></li>';
+	sHeaderMenu.innerHTML = '<li><a href="/edu">Образование</a></li><li><a href="/projects">Проекты</a></li>';
 	sHeader.appendChild(sHeaderMenu);
 }
